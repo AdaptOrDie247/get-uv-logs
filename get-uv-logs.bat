54967f818832e8b8ecdbd0a4e6d436f94ab0a04d
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-03-06 23:43:16 -0800

    Update get-uv-logs.bat

--- get-uv-logs.bat
+++ get-uv-logs.bat
@@ -9,9 +9,8 @@
 
 :: Usage:
 :: 1. Copy get-uv-logs.bat to an empty USB drive.
-:: 2. Open USB drive in File Explorer on target host.
-:: 3. Right click get-uv-logs.bat then click "Run as administrator".
-:: 4. Files and info will be copied to the appropriate destination directories.
+:: 2. Run get-uv-logs.bat from USB drive on target host as victim user account of incident.
+:: 3. Files and info will be copied to the appropriate destination directories.
 
 :: Notes:
 :: - Tested using install (exe) version of UltraViewer 6.6 on Windows 10 Pro.
